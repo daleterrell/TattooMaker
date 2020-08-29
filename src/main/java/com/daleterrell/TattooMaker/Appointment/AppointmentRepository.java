88b2601fd814857385330aeca101ed6f829f43c7
Dale Terrell <tragicid@gmail.com>
2020-08-29 15:44:59 -0500

    no

--- src/main/java/com/daleterrell/TattooMaker/Appointment/AppointmentRepository.java
+++
@@ -1,5 +0,0 @@
-package com.daleterrell.TattooMaker.Appointment;
-
-public class AppointmentRepository {..
-
-}
--- src/main/java/com/daleterrell/TattooMaker/Artist/ArtistRepository.java
+++
@@ -1,5 +0,0 @@
-package com.daleterrell.TattooMaker.Artist;
-
-public class ArtistRepository {
-
-}
